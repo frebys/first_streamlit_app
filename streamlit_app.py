e8fbbb9616e801cad267755e7e7288442b2d3346
Frebby Soegianto <116367956+frebys@users.noreply.github.com>
2022-10-23 20:25:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,21 +28,16 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-streamlit.error("Please select a fruit to get information.")
-else
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize (fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-#streamlit.write('the user entered', fruit_choice)
-#import requests
-# take the json version of the response and normalize it
-#fruityvice_normalized = pandas.json_normalize (fruityvice_response.json())
-# output it as table on screen
-#streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-streamlit.error()
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+   else:
+       fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+       fruityvice_normalized = pandas.json_normalize (fruityvice_response.json())
+       streamlit.dataframe(fruityvice_normalized)
+
+ except URLError as e:
+    streamlit.error()
 
 #don't run anything
 streamlit.stop()
